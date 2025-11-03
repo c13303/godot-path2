faaac1b176e5a6af9c1714484b460076932d5631
Charles homedesktop <erreure@gmail.com>
2025-11-03 10:06:53 +0100

    update

--- character/character.gd
+++ character/character.gd
@@ -11,6 +11,11 @@ var has_reserved: bool = false  #pour destinations
 var has_last_cell: bool = false
 var reserved_cell: Vector2i = Vector2i.ZERO
 var last_cell: Vector2i = Vector2i.ZERO
+var myGoal: Vector2
+var myDecalGoal: Vector2 = Vector2.ZERO
+var isDecaling: bool = false
+var delaiDecaling = 0
+var current_dir: Vector2 = Vector2.ZERO
 
 
 func _ready() -> void:
@@ -20,15 +25,21 @@ func _ready() -> void:
 func _unhandled_input(event: InputEvent) -> void:
 	# Détection du clic gauche de la souris
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-		# Vérifie qu’un gestionnaire de chemin existe
-		if path_manager == null:
-			return
-
-		# Détermine la position de départ (centrée sur le personnage)
-		var start: Vector2 = global_position + Vector2(0, offset_y)
-
 		# Détermine la position de destination selon la souris
-		var goal: Vector2 = get_global_mouse_position()
+		myGoal = get_global_mouse_position()
+		_calcule_chemin(myGoal)
+
+		
+func _calcule_chemin(goal: Vector2): # CALCULE LE CHEMIN DE SA POSITION SUR LA MAP ASTAR UNIQUEMENT + position libre d'agent la plus proche
+	#ne va pas éviter les agents en mouvement
+	 
+	# Détermine la position de départ (centrée sur le personnage)
+		var start: Vector2 = global_position + Vector2(0, offset_y)
+		
+		if(!goal):
+			print("ERROR NO GOAL")
+			return
+		
 
 		# Récupère le pathfinder et le calque du sol
 		var pf = path_manager.pathfinder
@@ -68,17 +79,45 @@ func _unhandled_input(event: InputEvent) -> void:
 		# Envoie une requête de calcul de chemin au pathfinder
 		path_manager.call("request_path", start, goal_pos, func(p): _on_path_ready(p))
 
-
 func _on_path_ready(p: PackedVector2Array) -> void:
 	path = p
 	current_waypoint = 0
+	
+func _end_of_movement(clearPath : bool = false):
+	if(clearPath):
+		path.clear()
+	velocity = Vector2.ZERO
+	move_and_slide()
+	z_index = int(global_position.y)
+	
+func _trig_decal():
+	var pos: Vector2 = global_position + Vector2(0, offset_y)
+	var free = path_manager.find_nearest_free_cell(pos,1,current_dir)
+	if free != null:
+		_calcule_chemin(free)			
+		isDecaling = true
+		return true
+	else:
+		return false
 
 func _physics_process(delta: float) -> void:
-	if path.is_empty() or current_waypoint >= path.size():		
-		velocity = Vector2.ZERO
-		move_and_slide()
-		z_index = int(global_position.y)
+	
+	if isDecaling:
+		if delaiDecaling > 0:
+			return
+		else:
+			delaiDecaling -= delta * 1000.0
+			if delaiDecaling <= 0:
+				_trig_decal()
+			return
+	
+	if path.is_empty():
+		_end_of_movement(true);
 		return
+	
+	if current_waypoint >= path.size():
+		_end_of_movement(true);
+		return;
 
 	var pos: Vector2 = global_position + Vector2(0, offset_y)
 	var target_pos: Vector2 = path[current_waypoint]
@@ -87,30 +126,38 @@ func _physics_process(delta: float) -> void:
 	if pos.distance_to(target_pos) <= arrival_threshold:
 		current_waypoint += 1
 		# dernier waypoint atteint
-		if current_waypoint >= path.size():
-			path.clear()
+		if current_waypoint >= path.size():			
 			# Marque la cellule finale comme occupée (agent statique)
 			var final_cell: Vector2i = path_manager.pathfinder.world_to_cell(pos)
 			path_manager.occupy_cell(final_cell, self)
 			last_cell = final_cell
 			has_last_cell = true
+			_end_of_movement(true)			
 			
-			velocity = Vector2.ZERO
-			move_and_slide()
-			z_index = int(global_position.y)
+			#goal temporaire de decal
+			if isDecaling == true:
+				isDecaling = false
+				_calcule_chemin(myGoal)
 			return
 		target_pos = path[current_waypoint]
 
 	var dir: Vector2 = (target_pos - pos).normalized()
+	if dir.length() > 0.01:
+		current_dir = Vector2i(sign(dir.x), sign(dir.y))
+	
+	
+	
 	var tile_size: Vector2 = path_manager.pathfinder.floor_layer.tile_set.tile_size
 	var next_pos: Vector2 = pos + dir * speed * delta
 	var next_cell: Vector2i = path_manager.pathfinder.world_to_cell(next_pos)
 
 	# Feu rouge : s'arrêter seulement si la cellule suivante est occupée par un autre agent
 	if path_manager.is_cell_occupied(next_cell, self):
-		velocity = Vector2.ZERO
-		move_and_slide()
-		z_index = int(global_position.y)
+		#trouver une case libre (!= occupied) condition : maximum 1 tile de distance du perso, préférence la plus proche de cell suivante		
+	
+		delaiDecaling = 10		
+		_end_of_movement(false)
+			
 		return
 
 	# Mise à jour des cellules
