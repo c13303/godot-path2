5bdd8787a7f4557e73ad85f2c6faaf0e27f7c699
Charles homedesktop <erreure@gmail.com>
2026-04-28 15:47:06 +0200

    keepGodot451

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /c/CHARLES/DEV/GODOTPROJS/FLOWFIELD_CPP/projet_godot
+cd /c/CHARLES/DEV/GODOTPROJS/RABBITGAME/dagodot-rabbitgame
 echo "=== Compilation ==="
 if ! scons -C extensions target=template_debug use_mingw=yes; then
     echo "=== Compilation échouée ==="
@@ -9,5 +9,5 @@ if ! scons -C extensions target=template_debug use_mingw=yes; then
 fi
 
 echo "=== Lancement de Godot ==="
-/c/CHARLES/DEV/GODOTPROJS/FLOWFIELD_CPP/godot451/Godot_v4.5.1-stable_win64.exe \
-  --path "/c/CHARLES/DEV/GODOTPROJS/FLOWFIELD_CPP/projet_godot"
+/c/CHARLES/DEV/GODOTPROJS/RABBITGAME/engine/Godot_v4.5.1-stable_win64.exe \
+  --path "/c/CHARLES/DEV/GODOTPROJS/RABBITGAME/dagodot-rabbitgame"
