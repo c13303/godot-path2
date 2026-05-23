34941a749d9ca0787d854fe311a7277a5fa24509
Charles homedesktop <erreure@gmail.com>
2026-05-23 17:11:19 +0200

    save

--- label.gd
+++ label.gd
@@ -5,7 +5,10 @@ var hover_cell_text := ""
 func _process(_delta: float) -> void:
 	var sb := ""
 	sb += "FPS: %d\n" % Engine.get_frames_per_second()
-	sb += "Agents: %d\n" % get_tree().get_nodes_in_group("main_chars").size()
+	var agent_count := 0
+	for group_name in ["main_chars", "monsters", "player"]:
+		agent_count += get_tree().get_nodes_in_group(group_name).size()
+	sb += "Agents: %d\n" % agent_count
 	if hover_cell_text != "":
 		sb += hover_cell_text
 	text = sb
